a36146206a39b66cca3fb322ec5008cd85500ea9
daw <daw@daw-PC08>
2017-03-31 10:17:08 +0200

    directorios listado casi listo

--- src/manejadirectorios/FXMLDocumentController.java
+++ src/manejadirectorios/FXMLDocumentController.java
@@ -10,10 +10,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
-import javafx.scene.control.TreeView;
 import javafx.scene.layout.Pane;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import modelo.AccionesDirectorio;
@@ -48,13 +49,14 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Button botonEjecutar;
     @FXML
-    private TreeView<?> fieldTree;
-    @FXML
     private Button botonSalir;
     @FXML
     private TextField fiendRuta;
+    @FXML
+    private TextArea textZone;
 
     private AccionesDirectorio acciones = new AccionesDirectorio();
+    
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -64,24 +66,27 @@ public class FXMLDocumentController implements Initializable {
         fieldTamaño.setStyle("-fx-background-color : lightgreen;");
         fiendRuta.setStyle("-fx-background-color : lightgreen;");
         panelRadio.setStyle("-fx-border-color:black; -fx-border-width:1px; -fx-border-radius:3px;");
-        fieldTree.setStyle("-fx-background-color: darkgray; -fx-font-color:white;");
+        
 
     }
 
     @FXML
     private void abrirRuta(ActionEvent event) {
-       FileChooser fileChooser = new FileChooser();
+        Stage stage = new Stage();
+       DirectoryChooser fileChooser = new DirectoryChooser();
         fileChooser.setTitle("Buscar Archivo");
-        File archivo = fileChooser.showOpenDialog(null);
+        File archivo = fileChooser.showDialog(stage);
         if (archivo != null) {
             File pathArchivo = new File(archivo.getAbsolutePath());
             acciones.setDirectorio(pathArchivo.toPath());
-            
             fiendRuta.setText(String.valueOf(pathArchivo));
     }
     }
     @FXML
     private void ejecutar(ActionEvent event) {
+        if (listarContenido.isSelected()){
+            textZone.setText(String.valueOf(acciones.listarDirectorio()));
+        }
     }
 
     @FXML
--- src/modelo/AccionesDirectorio.java
+++ src/modelo/AccionesDirectorio.java
@@ -1,15 +1,18 @@
-
 package modelo;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-
+import java.util.Iterator;
+import java.util.stream.Stream;
 
 public class AccionesDirectorio {
+
     private Path directorio;
 
     public AccionesDirectorio() {
-        
+
     }
 
     public Path getDirectorio() {
@@ -19,10 +22,35 @@ public class AccionesDirectorio {
     public void setDirectorio(Path directorio) {
         this.directorio = directorio;
     }
-    public String[] listarDirectorio(){
-         File listado =directorio.toFile();
-         String[] lista =listado.list();
-         return lista;
+
+    public String listarDirectorio() {
+        //File listado =directorio.toFile();
+        String lista = "";
+        lista = "1. LISTADO DEL CONTENIDO DE LA CARPETA: " + this.directorio.getFileName() + "\n";
+
+        try (Stream<Path> ficheros = Files.list(directorio)) {
+            Iterator<Path> it = ficheros.iterator();
+
+            while (it.hasNext()) {
+                Path tipo = it.next();
+                File tipos = tipo.toFile();
+                if (tipos.isDirectory()) {
+                    lista += "CARPETA " + tipos.getName() + " TAMAÑO "+ tipos.length() + " bytes " + "\n";
+                } else {
+                    lista += "ARCHIVO " + tipos.getName() + " TAMAÑO "+ tipos.length() + " bytes " + "\n";
+                }
+            }
+                }catch (IOException ex) {
+            lista = "Error en la lista de ficheros";
     }
-    
-}
+                return lista;
+
+            }
+
+        }
+     
+
+            
+      
+ 
+
