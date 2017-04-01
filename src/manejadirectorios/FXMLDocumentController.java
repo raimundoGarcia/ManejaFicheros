01f1644f593eb40a97665b0a0348e92bbc7319ee
Shingo <Shingo@Shingo-PC>
2017-04-02 01:59:26 +0200

    done

--- src/manejadirectorios/FXMLDocumentController.java
+++ src/manejadirectorios/FXMLDocumentController.java
@@ -56,41 +56,99 @@ public class FXMLDocumentController implements Initializable {
     private TextArea textZone;
 
     private AccionesDirectorio acciones = new AccionesDirectorio();
-    
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        panelBase.setStyle("-fx-background-color: lightgrey;");
-        fieldFiltrado.setStyle("-fx-background-color : lightgreen;");
-        fieldNuevoArchivo.setStyle("-fx-background-color : lightgreen;");
-        fieldTamaño.setStyle("-fx-background-color : lightgreen;");
-        fiendRuta.setStyle("-fx-background-color : lightgreen;");
-        panelRadio.setStyle("-fx-border-color:black; -fx-border-width:1px; -fx-border-radius:3px;");
-        
+        estilos();
+        accesos();
 
     }
 
     @FXML
     private void abrirRuta(ActionEvent event) {
         Stage stage = new Stage();
-       DirectoryChooser fileChooser = new DirectoryChooser();
+        DirectoryChooser fileChooser = new DirectoryChooser();
         fileChooser.setTitle("Buscar Archivo");
         File archivo = fileChooser.showDialog(stage);
         if (archivo != null) {
             File pathArchivo = new File(archivo.getAbsolutePath());
             acciones.setDirectorio(pathArchivo.toPath());
             fiendRuta.setText(String.valueOf(pathArchivo));
+        }
+        accesos();
     }
-    }
+
     @FXML
     private void ejecutar(ActionEvent event) {
-        if (listarContenido.isSelected()){
-            textZone.setText(String.valueOf(acciones.listarDirectorio()));
+        if (acciones.getDirectorio() != null) {
+            if (listarContenido.isSelected()) {
+                textZone.setText(acciones.listarDirectorio());
+            } else if (listarFiltrado.isSelected()) {
+                textZone.setText(acciones.filtrarContenido(fieldFiltrado.getText()));
+            } else if (listarLectura.isSelected()) {
+                textZone.setText(acciones.archivosLectura());
+            } else if (listarTamaño.isSelected()) {
+                textZone.setText(acciones.contenidoPorTamaño(Long.parseLong(fieldTamaño.getText())));
+            } else if (crearNuevo.isSelected()) {
+                acciones.crearArchivo(fieldNuevoArchivo.getText());
+            } else {
+                textZone.setText("Elige una opción");
+            }
+        } else {
+            textZone.setText("Abre antes un directorio");
         }
     }
 
     @FXML
     private void salir(ActionEvent event) {
+        
+        
     }
 
+    private void estilos() {
+        panelBase.setStyle("-fx-background-color: lightgrey;");
+        fieldFiltrado.setStyle("-fx-background-color : lightgreen;");
+        fieldNuevoArchivo.setStyle("-fx-background-color : lightgreen;");
+        fieldTamaño.setStyle("-fx-background-color : lightgreen;");
+        fiendRuta.setStyle("-fx-background-color : lightgreen;");
+        panelRadio.setStyle("-fx-border-color:black; -fx-border-width:1px; -fx-border-radius:3px;");
+        fieldFiltrado.setVisible(false);
+        fieldTamaño.setVisible(false);
+        fieldNuevoArchivo.setVisible(false);
+        textZone.setStyle("-fx-control-inner-background:black;");
+    }
+
+    private void accesos() {
+        if (acciones.getDirectorio() == null) {
+            RadioButton[] buttons = new RadioButton[]{listarContenido, listarFiltrado, listarLectura, listarTamaño, crearNuevo};
+            for (RadioButton btn : buttons) {
+                btn.setDisable(true);
+            }
+        }
+        if (acciones.getDirectorio() != null) {
+            RadioButton[] buttons = new RadioButton[]{listarContenido, listarFiltrado, listarLectura, listarTamaño, crearNuevo};
+            for (RadioButton btn : buttons) {
+                btn.setDisable(false);
+            }
+        }
+    }
+
+    @FXML
+    private void select(ActionEvent event) {
+        if (listarFiltrado.isSelected()) {
+            fieldFiltrado.setVisible(true);
+        } else {
+            fieldFiltrado.setVisible(false);
+        }
+        if (listarTamaño.isSelected()) {
+            fieldTamaño.setVisible(true);
+        } else {
+            fieldTamaño.setVisible(false);
+        }
+        if (crearNuevo.isSelected()) {
+            fieldNuevoArchivo.setVisible(true);
+        } else {
+            fieldNuevoArchivo.setVisible(false);
+        }
+    }
 }
--- src/modelo/AccionesDirectorio.java
+++ src/modelo/AccionesDirectorio.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.text.SimpleDateFormat;
 import java.util.Iterator;
 import java.util.stream.Stream;
 
@@ -24,7 +25,7 @@ public class AccionesDirectorio {
     }
 
     public String listarDirectorio() {
-        //File listado =directorio.toFile();
+
         String lista = "";
         lista = "1. LISTADO DEL CONTENIDO DE LA CARPETA: " + this.directorio.getFileName() + "\n";
 
@@ -35,22 +36,120 @@ public class AccionesDirectorio {
                 Path tipo = it.next();
                 File tipos = tipo.toFile();
                 if (tipos.isDirectory()) {
-                    lista += "CARPETA " + tipos.getName() + " TAMAÑO "+ tipos.length() + " bytes " + "\n";
+                    lista += "CARPETA " + tipos.getName() + ", TAMAÑO " + conversorTamaño(tipos.length())
+                            + ", Modificado " + conversorFecha(tipos.lastModified()) + "\n";
                 } else {
-                    lista += "ARCHIVO " + tipos.getName() + " TAMAÑO "+ tipos.length() + " bytes " + "\n";
+                    lista += "ARCHIVO " + tipos.getName() + " TAMAÑO " + conversorTamaño(tipos.length())
+                            + ", Modificado " + conversorFecha(tipos.lastModified()) + "\n";
                 }
             }
-                }catch (IOException ex) {
+        } catch (IOException ex) {
             lista = "Error en la lista de ficheros";
-    }
-                return lista;
+        }
+        return lista;
 
+    }
+
+    private String conversorTamaño(Long tamaño) {
+        String unidad;
+        if (tamaño / 1000000000 > 1) {
+            unidad = tamaño / 1000000000 + " GB";
+        } else if (tamaño / 1000000 > 1) {
+            unidad = tamaño / 1000000 + " MB";
+        } else if (tamaño / 1000 > 1) {
+            unidad = tamaño / 1000 + " KB";
+        } else {
+            unidad = tamaño + " Bytes";
+        }
+        return unidad;
+    }
+
+    private String conversorFecha(long milisegundos) {
+        String fecha;
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy ; HH:mm:ss");
+        fecha = formato.format(milisegundos);
+        return fecha;
+    }
+
+    public String filtrarContenido(String cadena) {
+        String contenido = "";
+
+        try (Stream<Path> ficheros = Files.list(directorio)) {
+            Iterator<Path> it = ficheros.iterator();
+
+            while (it.hasNext()) {
+                Path tipo = it.next();
+                if (tipo.getFileName().toString().contains(cadena)) {
+                    File tipos = tipo.toFile();
+                    if (tipos.isDirectory()) {
+                        contenido += "CARPETA " + tipos.getName() + ", TAMAÑO " + conversorTamaño(tipos.length())
+                                + ", Modificado " + conversorFecha(tipos.lastModified()) + "\n";
+                    } else {
+                        contenido += "ARCHIVO " + tipos.getName() + " TAMAÑO " + conversorTamaño(tipos.length())
+                                + ", Modificado " + conversorFecha(tipos.lastModified()) + "\n";
+                    }
+                }
             }
+        } catch (IOException ex) {
+            contenido = "Error en la lista de ficheros";
+        }
+        return contenido;
+    }
+
+    public String archivosLectura() {
+        String lista = "";
+        try (Stream<Path> ficheros = Files.list(directorio)) {
+            Iterator<Path> it = ficheros.iterator();
+
+            while (it.hasNext()) {
+                Path tipo = it.next();
+                File tipos = tipo.toFile();
+
+                if (tipos.isFile()) {
+                    lista += tipos.getName() + " TAMAÑO " + conversorTamaño(tipos.length())
+                            + ", Modificado " + conversorFecha(tipos.lastModified()) + "\n";
+                }
+            }
+        } catch (IOException ex) {
+            lista = "Error en la lista de ficheros";
+        }
+        return lista;
+
+    }
+
+    public String contenidoPorTamaño(long bytes) {
+        String lista = "";
+        try (Stream<Path> ficheros = Files.list(directorio)) {
+            Iterator<Path> it = ficheros.iterator();
+
+            while (it.hasNext()) {
+                Path tipo = it.next();
+                File tipos = tipo.toFile();
+                if (tipos.length() == bytes) {
+                    if (tipos.isDirectory()) {
+                        lista += "CARPETA " + tipos.getName() + ", TAMAÑO " + conversorTamaño(tipos.length())
+                                + ", Modificado " + conversorFecha(tipos.lastModified()) + "\n";
+                    } else {
+                        lista += "ARCHIVO " + tipos.getName() + " TAMAÑO " + conversorTamaño(tipos.length())
+                                + ", Modificado " + conversorFecha(tipos.lastModified()) + "\n";
+                    }
+                }
+            }
+        } catch (IOException ex) {
+            lista = "Error en la lista de ficheros";
+        }
+        return lista;
+    }
+
+    public void crearArchivo(String nuevo) {
+        try {
+            File nuevos = new File(directorio +"\\"+ nuevo);
+            if (!nuevos.exists()) {
+                nuevos.createNewFile();
+            }
+            
+        } catch (IOException ex) {
 
         }
-     
-
-            
-      
- 
-
+    }
+}
